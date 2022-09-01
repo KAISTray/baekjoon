80c778eba06d5bc07877b82fa54237a236905c48
ray_notebook <jw1027@kaist.ac.kr>
2022-09-02 04:20:52 +0900

    1005 수정
    근데 다시 다 짜야할듯 ㅅㅂ

--- BaekJoon OJ/BOJ1005.cpp
+++ BaekJoon OJ/BOJ1005.cpp
@@ -15,13 +15,13 @@ int main() {
 		int n,k;
 		scanf("%d %d", &n, &k);
 		
-		int d[n]; //time to build
+		int d[n+1]; //time to build
 		
-		for (i=0; i<n; i++) {
+		for (i=1; i<=n; i++) {
 			scanf("%d", &d[i]);
 		}
 		
-		vector<int> a[n];
+		vector<vector<int> > a(n+1);
 		int init, end;
 		
 		for (i=0; i<k; i++) {
@@ -33,24 +33,53 @@ int main() {
 		deque<int> valid; //유효한 끝노드 인덱스들 
 		deque<int> visit; //방문할 덱들 
 		
-		int W, now;
+		int W, now, cur;
 		scanf("%d", &W);
 		
 		visit.push_front(W);
 		
-		while(true) { //순회버스 출발
+		while(!visit.empty()) { //순회버스 출발
 		// 정점 W에서 시작해서 주변 하나씩 탐색하기 
 			
+			now = visit.back();
+			visit.pop_back();
 			
-			now = visit.pop_back();
-			//visit_pop_back을 뽑아옴
-			//그 근처에 애들 다 순회함
-			while(true) {
-				if (min[n] == 0) {
-					
-				} 
+			if (visited[now] == 1) {
+				continue;
 			}
 			
+			//visit_pop_back을 뽑아옴
+			//그 근처에 애들 다 순회함
+			if (a[now].empty()) {
+				valid.push_back(now);
+			}
+			
+			while(!a[now].empty()) {
+				cur = a[now].back();
+				a[now].pop_back();
+				
+				if (min[cur] == 0 || min[cur] < min[now] + d[cur]) {
+					min[cur] = min[now] + d[cur];
+				}
+				
+				if (visited[now] != 1) {
+					visit.push_front(cur);
+				}
+			}
+			visited[now] = 1;
 		}
+		
+		int Ans = -1;
+		
+		
+		while(!valid.empty()) {
+			cur = valid.back();
+			valid.pop_back();
+			if (Ans == -1 || Ans > min[cur]) {
+				Ans = min[cur];
+			}
+		}
+		
+		printf("%d\n", Ans);
 	}
 }
