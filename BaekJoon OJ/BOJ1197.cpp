caf4e208bb1c60fe4e8ad42f5dab75f62469d939
ray_notebook <jw1027@kaist.ac.kr>
2022-09-02 23:56:25 +0900

    make 1197 file
    asdf

---
+++ BaekJoon OJ/BOJ1197.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int v, e;
+	scanf("%d %d", &v, &e);
+	
+	vector<tuple<int, int, int> > edge;
+	
+	for (int i=0; i<e; i++) {
+		int i,f,d;
+		scanf("%d %d %d", &i, &f, &d);
+		
+	}
+}
